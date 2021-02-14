8631b092f5e6ebd9f2fd3ca56cf9f1a604889ba0
riosusanto <riosst100@gmail.com>
2021-02-14 10:00:23 +0700

    Add manifest

--- components/Layout.js
+++ components/Layout.js
@@ -9,6 +9,7 @@ export default function Layout(props) {
           <title>{props.title}</title>
           <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
           <link rel="icon" href="https://brebes-social.id/assets/images/logo/cira.webp" />
+          <link rel="manifest" href="/manifest.json"></link>
         </Head>
         <Navbar />
         <div style={{ background: '#edf2f7', padding: '0px' }}>
