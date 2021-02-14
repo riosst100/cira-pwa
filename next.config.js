897a97e7995e8a53c14dd534f49900dc023b2abd
riosst <riosst100@gmail.com>
2021-02-14 11:24:10 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
     env: {
-        // MONGO_URI: "mongodb+srv://Rio991155:Rio991155@cluster0.bjhqg.mongodb.net/ciraapp",
-        MONGO_URI: "mongodb://localhost:27017/ciraapp",
-        BASE_URL: "http://localhost:3000"
-        // BASE_URL: "https://pwa-cira.vercel.app"
+        MONGO_URI: "mongodb+srv://Rio991155:Rio991155@cluster0.bjhqg.mongodb.net/ciraapp",
+        BASE_URL: "https://pwa-cira.vercel.app"
     }
 }
