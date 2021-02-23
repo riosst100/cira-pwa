0fe2ee910ac9d28a237cb192e29e22370c111621
riosusanto <riosst100@gmail.com>
2021-02-23 18:53:32 +0700

    Ably websocket

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
         MONGO_URI: "mongodb+srv://Rio991155:Rio991155@cluster0.bjhqg.mongodb.net/ciraapp",
-        BASE_URL: "https://pwa-cira.vercel.app"
+        BASE_URL: "https://pwa-cira.vercel.app",
+        ABLY_API_KEY: "W6q93w.g61EsA:cvct_bCFY3ns8nUe"
     }
 }
