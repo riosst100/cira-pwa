79e52a4e84e19d81645504a78cafaffe2d904c75
Rio Susanto <riosst100@gmail.com>
2021-04-15 18:51:50 +0700

    Update 15/04/2021

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import { useCurrentUser } from '@/hooks/index';
-import PostEditor from '@/components/post/editor';
-import Posts from '@/components/post/posts';
 import Layout from '@/components/layout'
+import LoginRegister from '@/components/loginRegister'
 
 const IndexPage = () => {
   const [user] = useCurrentUser();
 
   return (
-      <Layout title="Cira App">
-        <div className="section content-section pt-1">
-          <img className="home-banner" src="http://brebes-social.id/public/images/banner/banner-cira.png"/>
-        </div>
-      <div style={{ marginBottom: '2rem' }}>
-        <h2>
-          Hello,
-          {' '}
-          {user ? user.name : 'pengunjung'}
-          !
-        </h2>
+    <Layout title="Cira App">
+      <div className="section content-section pt-1">
+        <img className="home-banner" src="http://brebes-social.id/public/images/banner/banner-cira.png"/>
       </div>
-      </Layout>
+      <LoginRegister user={user} />
+    </Layout>
   );
 };
 
