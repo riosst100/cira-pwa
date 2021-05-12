9866cfd19a65b4b6f9b0a69986c1e74291240840
Rio Susanto <riosst100@gmail.com>
2021-05-12 13:43:54 +0700

    Update 12/05/2021

--- components/GlobalBlockNotif.jsx
+++ components/GlobalBlockNotif.jsx
@@ -7,7 +7,7 @@ export default function GlobalBlockNotif({ user }) {
 
   if (!user) {
     loginStatus = (<div className="section m-2" style={{
-        "box-shadow": "0 1px 2px 0 rgb(0 0 0 / 22%)"
+        "box-shadow": "rgb(0 0 0 / 12%) 0px 1px 3px 0px"
     }}>
     <div className="content text-center">
         <div style={{
@@ -16,9 +16,8 @@ export default function GlobalBlockNotif({ user }) {
             "backgroundColor": "white",
             "color":"white"
         }}>
-            <div style={{"backgroundColor":"#009effa3"}}>
-      <div><b>Info Terbaru!</b></div>
-      <div className="pb-2">
+            <div style={{"backgroundColor":"#009effb0"}}>
+      <div className="p-2" style={{"textShadow":"1px 1px 2px #00000085"}}>
           Selamat hari raya Idul Fitri 1442 H!
       </div>
       </div>
