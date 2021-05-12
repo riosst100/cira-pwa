9ac159c5fcacdb989e4b65fffba5b72f13c70765
Rio Susanto <riosst100@gmail.com>
2021-05-12 12:14:48 +0700

    Update 12/05/2021

---
+++ components/GlobalBlockNotif.jsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { useEffect, useState } from 'react'
+
+export default function GlobalBlockNotif({ user }) {
+  let loginStatus = '';
+
+  if (!user) {
+    loginStatus = (<div className="section m-2">
+    <div className="content text-center">
+        <div style={{
+            "background": "url(https://brebes-social.id/images/overlay/idul-fitri.webp)",
+            "backgroundSize": "contain",
+            "backgroundColor": "white",
+            "color":"white"
+        }}>
+            <div style={{"backgroundColor":"#009effa3"}}>
+      <div><b>Info Terbaru!</b></div>
+      <div className="pb-2">
+          Selamat hari raya Idul Fitri 1442 H!
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>);
+  }
+
+  return loginStatus;
+}
