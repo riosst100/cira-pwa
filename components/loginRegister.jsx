ee4295ef88471f702be4d284787b9c482ab255ff
Rio Susanto <riosst100@gmail.com>
2021-05-12 18:41:26 +0700

    Update 12/05/2021

--- components/loginRegister.jsx
+++ components/loginRegister.jsx
@@ -1,33 +1,32 @@
 import Link from 'next/link'
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-import { isLogin } from '@/hooks/index';
+import useSWR from 'swr';
+import fetcher from '@/lib/fetch';
 
 export default function LoginRegister({ user }) {
-    const isUserLogin = isLogin();
-    const loginRegister = (
-    <>
+  const { data } = useSWR('/api/user', fetcher);
+  const loginRegister = (
     <div className="section mt-3 ml-2 mr-2">
-    <div className="content text-center p-2">
-      <div><b>Selamat Datang di Cira App!</b></div>
-      <div className="pb-2"><small>Silahkan Daftar atau Masuk jika sudah terdaftar.</small></div>
-      <div className="pb-1">
-        <Link href="/register">
-          <a>
-        <button className="mr-5 bg-primary hover:bg-green-700 text-white font-bold py-1 px-5 rounded focus:outline-none focus:shadow-outline">
-        <i className="fab fa-whatsapp"></i> Daftar
-      </button>
-      </a>
-        </Link>
-        <Link href="/login">
-          <a><button className="ml-5 bg-primary hover:bg-green-700 text-white font-bold py-1 px-5 rounded focus:outline-none focus:shadow-outline">
-        <i className="fab fa-whatsapp"></i> Masuk
-      </button></a>
-        </Link>
+      <div className="content text-center p-2">
+        <div><b>Selamat Datang di Cira App!</b></div>
+        <div className="pb-2"><small>Silahkan Daftar atau Masuk jika sudah terdaftar.</small></div>
+        <div className="pb-1">
+          <Link href="/register">
+            <a>
+          <button className="mr-5 bg-primary hover:bg-green-700 text-white font-bold py-1 px-5 rounded focus:outline-none focus:shadow-outline">
+          <i className="fab fa-whatsapp"></i> Daftar
+        </button>
+        </a>
+          </Link>
+          <Link href="/login">
+            <a><button className="ml-5 bg-primary hover:bg-green-700 text-white font-bold py-1 px-5 rounded focus:outline-none focus:shadow-outline">
+          <i className="fab fa-whatsapp"></i> Masuk
+        </button></a>
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
-  </>
   );
 
-  return isUserLogin == false ? loginRegister : <SkeletonTheme color="#e5e5e5" highlightColor="#e9e9e9"><div className="p-2"><Skeleton height={100} /></div></SkeletonTheme>;
+  return data ? !data.user && loginRegister : <SkeletonTheme color="#e5e5e5" highlightColor="#e9e9e9"><div className="p-2"><Skeleton height={100} /></div></SkeletonTheme>;
 }
