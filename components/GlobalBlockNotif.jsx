c33ad3f983f31b91b9a147ed9ac8a82024deb546
Rio Susanto <riosst100@gmail.com>
2021-06-07 18:21:04 +0700

    Update 07/06/2021

--- components/GlobalBlockNotif.jsx
+++ components/GlobalBlockNotif.jsx
@@ -6,27 +6,27 @@ export default function GlobalBlockNotif({ user }) {
   let loginStatus = '';
 
   if (!user) {
-    loginStatus = (<div className="section mt-1 mb-1">
-    <div className="content text-center p-1" style={
-      {
-        "borderRadius":"0px",
-        "borderTop": "1px solid #DCDCE9",
-        "borderBottom": "1px solid #DCDCE9"
-      }
-    }>
-        <div style={{
-            "background": "url(https://brebes-social.id/images/overlay/idul-fitri.webp)",
-            "backgroundSize": "contain",
-            "backgroundRepeat": "no-repeat"
-        }}>
-      <div className="p-1">
-        <marquee>
-          Selamat hari raya Idul Fitri 1442 H!
-        </marquee>
-      </div>
-      </div>
-    </div>
-  </div>);
+  //   loginStatus = (<div className="section mt-1 mb-1">
+  //   <div className="content text-center p-1" style={
+  //     {
+  //       "borderRadius":"0px",
+  //       "borderTop": "1px solid #DCDCE9",
+  //       "borderBottom": "1px solid #DCDCE9"
+  //     }
+  //   }>
+  //       <div style={{
+  //           "background": "url(https://brebes-social.id/images/overlay/idul-fitri.webp)",
+  //           "backgroundSize": "contain",
+  //           "backgroundRepeat": "no-repeat"
+  //       }}>
+  //     <div className="p-1">
+  //       <marquee>
+  //         Selamat hari raya Idul Fitri 1442 H!
+  //       </marquee>
+  //     </div>
+  //     </div>
+  //   </div>
+  // </div>);
   }
 
   return loginStatus;
--- components/pageBanner.jsx
+++ components/pageBanner.jsx
@@ -20,10 +20,10 @@ export default function PageBanner({ page }) {
     }
     return (
         <>
-            <div className="section content-section mb-1">
+            <div className="section content-section mb-1 mt-1">
                 {/* {isImageReady || <div className="home-banner"><Skeleton height={150} /></div>} */}
                 {/* style={{"display": isImageReady ? "block" : "none !important"}} */}
-                <img className="home-banner" src={banner} width={"100%"} height={150} />
+                <img className="home-banner" src={banner} width={"100%"} style={{"height":"150px"}} />
             </div>
         </>
     );
