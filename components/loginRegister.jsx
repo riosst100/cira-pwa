e405ad00165f2a7e35c3b5c9d73e792715106e86
Rio Susanto <riosst100@gmail.com>
2021-06-11 21:58:31 +0700

    Update 11/06/2021

--- components/loginRegister.jsx
+++ components/loginRegister.jsx
@@ -6,8 +6,7 @@ import fetcher from '@/lib/fetch';
 export default function LoginRegister({ user }) {
   const { data } = useSWR('/api/user', fetcher);
   const loginRegister = (
-    <div className="section mb-1 mt-1" style={{"border": "1px solid #e9e9f1"}}>
-      <div className="content text-center p-2">
+    <>
         <div><b>Selamat Datang di Cira App!</b></div>
         <div className="pb-2"><small>Silahkan Daftar atau Masuk jika sudah terdaftar.</small></div>
         <div className="pb-1">
@@ -24,9 +23,31 @@ export default function LoginRegister({ user }) {
         </button></a>
           </Link>
         </div>
-      </div>
-    </div>
+      </>
   );
 
-  return data ? !data.user && loginRegister : <SkeletonTheme color="#f1f2f6" highlightColor="#ffffff"><div className="mb-1"><Skeleton height={99} /></div></SkeletonTheme>;
+  return (
+    <>
+      <div className="section mb-1 mt-1" style={{"border": "1px solid #e9e9f1"}}>
+        <div className="content text-center p-2">
+          {
+            data ? !data.user && loginRegister : (
+              <SkeletonTheme color="#f1f2f6" highlightColor="#ffffff">
+                <div><Skeleton height={15} width="50%" /></div>
+                <div className="mt-1"><Skeleton height={10} width="70%" /></div>
+                <div className="mb-1">
+                  <span className="mr-5">
+                    <Skeleton height={25} width={75} />
+                  </span>
+                  <span className="ml-5">
+                    <Skeleton height={25} width={75} />
+                  </span>
+                </div>
+              </SkeletonTheme>
+            )
+          }
+        </div>
+      </div>
+    </>
+  );
 }
